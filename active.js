2486dc2938cb4da7f49eed295ac7189f2a71f789
NgocIsHere <lengoc.forworks@gmail.com>
2023-03-03 00:28:01 +0700

    0303version

---
+++ active.js
@@ -0,0 +1,16 @@
+function nameFunction() {
+    document.body.scrollTop = 100; // For Safari
+    document.documentElement.scrollTop = 100; // For Chrome, Firefox, IE and Opera
+}
+function infoFunction() {
+    document.body.scrollWidth = 500; // For Safari
+    document.documentElement.scrollTop = 500; // For Chrome, Firefox, IE and Opera
+}
+function degreeFunction() {
+    document.body.scrollTop = 1000; // For Safari
+    document.documentElement.scrollTop = 1000; // For Chrome, Firefox, IE and Opera
+}
+function aloFunction() {
+    document.body.scrollTop = 1500; // For Safari
+    document.documentElement.scrollTop = 1500; // For Chrome, Firefox, IE and Opera
+}
